b93c96518a086f8c85221419ff2a1771ef3bac8f
joelch97 <125751366+Wirreles@users.noreply.github.com>
2024-09-17 19:24:27 -0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,21 +6,21 @@
 export const environment = {
   production: false,
   firebase: {
-    // apiKey: "AIzaSyD8ap67gSGEXf2uuPFXeIX7unWMoGnnMC4",
-    // authDomain: "encuentro-8913c.firebaseapp.com",
-    // databaseURL: "https://encuentro-8913c-default-rtdb.firebaseio.com",
-    // projectId: "encuentro-8913c",
-    // storageBucket: "encuentro-8913c.appspot.com",
-    // messagingSenderId: "435048641453",
-    // appId: "1:435048641453:web:245f9cac9826f5c19a6724",
-    // measurementId: "G-XGXFGRB3F4",
-    apiKey: "AIzaSyBdwb8icJm8ibGiK1EautKz-Di3CGtYUKk",
-    authDomain: "pencuentro-8fdb3.firebaseapp.com",
-    projectId: "pencuentro-8fdb3",
-    storageBucket: "pencuentro-8fdb3.appspot.com",
-    messagingSenderId: "691492663327",
-    appId: "1:691492663327:web:ea9b187bbc1f603a62252a",
-    measurementId: "G-PQLTB04YXE"
+    apiKey: "AIzaSyD8ap67gSGEXf2uuPFXeIX7unWMoGnnMC4",
+    authDomain: "encuentro-8913c.firebaseapp.com",
+    databaseURL: "https://encuentro-8913c-default-rtdb.firebaseio.com",
+    projectId: "encuentro-8913c",
+    storageBucket: "encuentro-8913c.appspot.com",
+    messagingSenderId: "435048641453",
+    appId: "1:435048641453:web:245f9cac9826f5c19a6724",
+    measurementId: "G-XGXFGRB3F4",
+    // apiKey: "AIzaSyBdwb8icJm8ibGiK1EautKz-Di3CGtYUKk",
+    // authDomain: "pencuentro-8fdb3.firebaseapp.com",
+    // projectId: "pencuentro-8fdb3",
+    // storageBucket: "pencuentro-8fdb3.appspot.com",
+    // messagingSenderId: "691492663327",
+    // appId: "1:691492663327:web:ea9b187bbc1f603a62252a",
+    // measurementId: "G-PQLTB04YXE"
   },
   jwtSecret: "6gUjaumz67K/ItWGODOqJR83JMdOUMIGQN9D/y1Ms9Yt8UIFy3oU8mG+KNwFywC6ngwEtHFjhejBsU/Dntm3ll0rrL2oNywrFZmR33QLtH/NjV1pxc4wDV8dpfEp7iwISGTBBvhh0khSCdpPgY6hg3docSqxqCSteXgiwvroBTQk88z61yEPcVTZ1SguM2eQuquxycRcHr6NZivhmnQQ/Z5YU+gpV0aquOotNG/YGyR51sG0MtxGI6o6cjU28/S1kmv/n0LJr00q/yoQPtbq7m7lwwMr2+8iOPEtOi+FpqXCNfgfIhwXr8vZUo2hdZPBrietcJzQ+WJ917Sd5Z0OUw=="
 };
